cbe9241262c6b9dadc529a348dc658737abec073
Thiago Aderaldo Lessa - DATAPREVCE <thiago.lessa@dataprev.gov.br>
2024-08-16 13:50:47 -0300

    Criação de rotas para as páginas Home, Users e Products

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { Home } from "./pages/home/Home"
+import { Users } from "./pages/users/Users";
+import { Products } from "./pages/products/Products";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+       <Home />
+      ),
+    },
+    {
+      path: "users",
+      element: <Users />,
+    },
+    {
+      path: "products",
+      element: <Products />,
+    },
+  ]);
+
   return (
-    <div>Hello World</div>
+    <RouterProvider router={router} />
   )
 }
 
