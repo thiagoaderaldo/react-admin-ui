68401676d34d0ae02757c2265143ff51256ba379
Thiago Aderaldo Lessa <thiagoderaldo@gmail.com>
2024-08-31 15:24:49 -0300

    Navbar - implementação de ícones e estilo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,25 @@ import "./navbar.scss"
 
 export const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>React Admin UI</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="https://avatars.githubusercontent.com/u/5854180?v=4" alt="" />
+          <span>Thiago AL</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
   )
 }
